c1178025108b07cd82a1ca835d010151e42152b3
abdallahjbilou <abdallah.jbilou@gmail.com>
2016-11-11 10:43:29 +0100

    Add figaro - Protect my secret data in config/application.yml

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161110110328) do
+ActiveRecord::Schema.define(version: 20161110162643) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "cocktails", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.text "name"
   end
 
   create_table "doses", force: :cascade do |t|
@@ -32,9 +30,7 @@ ActiveRecord::Schema.define(version: 20161110110328) do
   end
 
   create_table "ingredients", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.text "name"
   end
 
   add_foreign_key "doses", "cocktails"
